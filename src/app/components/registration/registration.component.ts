23debdc6fed4ced54bf3134fdbb77a405448d3de
J Jones <jlssmmns93@gmail.com>
2018-12-28 08:23:01 -0500

    building password command in registration form

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms'
+import { from } from 'rxjs';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  private _registrationForm: FormGroup;
+
+  constructor(private _form: FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this._registrationForm = this._form.group({
+      email: new FormControl,
+      password: new FormControl,
+      confirmPassword: new FormControl
+    });
   }
 
 }
